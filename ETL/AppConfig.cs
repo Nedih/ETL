1b985184097f2af86b4f0f586e837719201b03ee
Nedih <ihor.nedaiev@gmail.com>
2025-04-01 11:56:43 +0300

    Cloud data download implemented

---
+++ ETL/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETL
+{
+    static class AppConfig
+    {
+        public static readonly string FileName = "data.csv";
+        public static readonly string FilePath = Path.Combine(Directory.GetCurrentDirectory(), FileName);
+        public static readonly string FileId = "1l2ARvh1-tJBqzomww45TrGtIh5j8Vud4";
+        public static readonly string CloudUrl = $"https://drive.google.com/uc?export=download&id={FileId}";
+    }
+}
--- ETL/Program.cs
+++ ETL/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System.Net;
+using ETL;
+
+DownloadCSV();
+
+Console.ReadKey();
+
+async void DownloadCSV()
+{
+    using HttpClient client = new HttpClient();
+    var response = await client.GetAsync(AppConfig.CloudUrl);
+
+    if (response.IsSuccessStatusCode)
+    {
+        var fileBytes = await response.Content.ReadAsByteArrayAsync();
+        await File.WriteAllBytesAsync(AppConfig.FileName, fileBytes);
+        Console.WriteLine($"File downloaded to: {AppConfig.FilePath}");
+    }
+    else
+    {
+        Console.WriteLine($"Error: {response.StatusCode}");
+    }
+}
