ad918298ba143d282b118446539b42c27aa80b3e
Nedih <ihor.nedaiev@gmail.com>
2025-04-01 11:02:44 +0300

    Add project files.

---
+++ ETL/CsvParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace ETL
+{
+    internal class CsvParser
+    {
+        void Parse(string location)
+        {
+            using (var reader = new StreamReader(location))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<Record>().ToList();
+            }
+        }
+    }
+}
